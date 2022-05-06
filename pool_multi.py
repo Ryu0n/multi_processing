ced11842320f2af9851e41bce81e6e3d257d1f1d
shawn <hsung951027@gmail.com>
2022-05-06 12:25:19 +0900

    starmap

--- pool_multi.py
+++ pool_multi.py
@@ -2,9 +2,16 @@ from multiprocessing import Pool
 
 
 def f(x):
-    return x*x
+    return x * x
+
+
+def f2(x, y):
+    return x + y
 
 
 if __name__ == '__main__':
     with Pool(processes=5) as p:
         print(p.map(f, [i for i in range(100000000)]))
+
+    with Pool(processes=2) as p:
+        print(p.starmap(f2, [(i, i) for i in range(10)]))
